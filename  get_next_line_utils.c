d8ea787cc211c8ac7507dcf56299799f920acc7a
lua <ltrevin-@student.42barcelona.com>
2024-02-28 14:38:19 +0100

    update firts utils

---
+++  get_next_line_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*    get_next_line_utils.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltrevin- <ltrevin-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/25 20:30:01 by ltrevin-          #+#    #+#             */
+/*   Updated: 2024/02/26 16:31:46 by ltrevin-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (unsigned char)c)
+		{
+			return ((char *)&s[i]);
+		}
+		i ++;
+	}
+	if ((unsigned char)c == '\0')
+	{
+		return ((char *)&s[i]);
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	size_t	s1len;
+	size_t	s2len;
+
+	s1len = ft_strlen(s1);
+	s2len = ft_strlen(s2);
+	join = malloc(sizeof(char) * (s1len + s2len + 1));
+	if (!join)
+		return (NULL);
+	ft_strlcpy(join, s1, s1len + 1);
+	ft_strlcpy(join + s1len, s2, s2len + 1);
+	return (join);
+}
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	src_size;
+
+	i = 0;
+	src_size = ft_strlen(src);
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < size - 1)
+		{
+			dest[i] = src[i];
+			i ++;
+		}
+		dest[i] = '\0';
+	}
+	return (src_size);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
