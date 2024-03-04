7cf7dd5484e2cc5ea32e51845452aa3e577d46bc
dhsdev <lua@gnomo.net>
2024-03-04 12:24:31 +0100

    Reestart and writed main logic

--- get_next_line.c
+++ get_next_line.c
@@ -5,49 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ltrevin- <ltrevin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/25 20:31:21 by ltrevin-          #+#    #+#             */
-/*   Updated: 2024/03/04 10:20:33 by ltrevin-         ###   ########.fr       */
+/*   Created: 2024/03/04 12:13:30 by ltrevin-          #+#    #+#             */
+/*   Updated: 2024/03/04 12:24:07 by ltrevin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_read(int fd, char *str)
+char	*read_line(int fd, char **saved)
 {
-	char	*buff;
-	int		by_read;
-	char	*line;
-	int 	i;
-	
-	buff = malloc(sizeof(char)*(BUFFER_SIZE+1));
-	//chech valid filedescriptor & buffersize
-	// si al read le pongo 0 no lee pero puedo comprobar si el fd es valid
-	if(fd < 0 || read(fd, buff, 0) < 0 || BUFFER_SIZE < 0)
-		return (0);
-	//check if line
-	if(!str)
-		return (NULL); // creo???
-	
-	// not sense rn pero lo tendra :'3
-	while (!(ft_strchr(str, '\n'))) // read until tengo salto de linea
-	{
-		if(by_read = read(fd, buff, BUFFER_SIZE) < 0) // petao
-			return (0);
-		while (buff[i] != '\n')
-			i++;
-		ft_strlcpy(line, buff, i); // copiar lo que sirve
-		i++;
-		ft_strlcpy(str, buff[i], ft_strlen(buff[i])); // guardar lo demas pa luego
-		return (line);
-	}
-	return (line)
+	// Read from file (check if it fails)
+	// Append the current read to what i had
+	// Check if newLine or EOF
+		// Save the rest in "saved" for later (idk if do it here or in get_next_line)
+		// Substring the line until newLine or EOF
+	// Return the line that i readed
 }
+
 
 char	*get_next_line(int fd)
 {
-	// comprovar cositas
-	//TODO: reorganizar caos (prioridad 1) mirar como y cuando hay q llamar a ft_read
-	// - intentar entender el bonus? se lo que hace pero no cómo 
-	// - clean ft_read
-	// - main to start checking things!
+	//Main Logic:
+	// Precheck if fd is valid
+	// Read line from file with read_line until newLine or EOF
+		// read_line
+		// save readed for next read (idk if do it here or in read_line)
+	// Return line
+
+	// TODO but idk where rn:
+		// Decide if i clean the line & save the rest in get_next_line or in read_line 
+			// (change declaration of read_line if is in gnl)
+		// Check if the read fails
+		// Check if EOF
+		// Handle multiple file descriptors (i thinks that my logic will do it?)
+	// TODO: write a main to check with multiple fd and not only files (4example stdinput?)
 }
