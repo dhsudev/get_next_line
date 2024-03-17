c0cce1706e254411848b950efb2bd9b4da58ac18
lua <ltrevin-@student.42barcelona.com>
2024-03-17 21:41:57 +0100

    segfaults por todos los lados 🎉

--- main.c
+++ main.c
@@ -9,17 +9,23 @@ int main(void)
   int  count;
 
   count = 0;
+  //printf("Init program\n");
   fd = open("file.txt", O_RDONLY);
-  while (1)
-  {
-    next_line = get_next_line(fd);
-    if (next_line == NULL)
-      break ;
-    count++;
-    printf("[%d]:%s\n", count, next_line);
-    next_line = NULL;
-  }
-  free(next_line);
+ // printf("File open succes!\n");
+  //while (1){
+	next_line = get_next_line(fd);
+	printf("[%d]:%s\n", count, next_line);
+	next_line = get_next_line(fd);
+	if (next_line == NULL)
+	{
+		printf("NULL RETURN\n");
+		//break ;
+	}
+    //count++;
+	printf("[%d]:%s\n", count, next_line);
+    //next_line = NULL;
+	//}
+  //free(next_line);
   close(fd);
   return (0);
 }
