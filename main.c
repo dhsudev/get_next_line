7f3f8b8d3e67b8a9d31e634194f8f4306057b113
lua <ltrevin-@student.42barcelona.com>
2024-03-31 22:38:02 +0200

    🦆cuack

--- main.c
+++ main.c
@@ -4,28 +4,25 @@
 
 int main(void)
 {
-  int    fd;
-  char  *next_line;
-  int  count;
+	int    fd;
+	char  *next_line;
+	int  count;
 
-  count = 0;
-  //printf("Init program\n");
-  fd = open("file.txt", O_RDONLY);
- // printf("File open succes!\n");
-  //while (1){
-	next_line = get_next_line(fd);
-	printf("[%d]:%s\n", count, next_line);
-	next_line = get_next_line(fd);
-	if (next_line == NULL)
-	{
-		printf("NULL RETURN\n");
-		//break ;
+	count = 0;
+	printf("Init program\n");
+	fd = open("file.txt", O_RDONLY);
+	printf("File open succes!\n");
+	while (1){
+		next_line = get_next_line(fd);
+		if (next_line == NULL)
+		{
+			printf("NULL RETURN\n");
+			break ;
+		}
+		printf("------------[%d]:%s.\n", count, next_line);
+		free(next_line);
+		count++;
 	}
-    //count++;
-	printf("[%d]:%s\n", count, next_line);
-    //next_line = NULL;
-	//}
-  //free(next_line);
-  close(fd);
-  return (0);
+	close(fd);
+	return (0);
 }
