742a8ac35a10654ccecb1306275b5cb4eff0b28a
dhsdev <lua@gnomo.net>
2024-05-05 20:22:52 +0200

    fixed all 🎷🦦

--- get_next_line.c
+++ get_next_line.c
@@ -6,137 +6,125 @@
 /*   By: ltrevin- <ltrevin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:36:35 by ltrevin-          #+#    #+#             */
-/*   Updated: 2024/04/29 15:28:53 by ltrevin-         ###   ########.fr       */
+/*   Updated: 2024/05/05 20:21:54 by ltrevin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <fcntl.h>
 
-void	lst_show(t_list **lst)
+void	read_fd(int fd, t_list **lst)
 {
-	t_list	*temp;
-	int		i;
+	char	*buffer;
+	int		bytes_read;
 
-	i = 0;
-	temp = *lst;
-	while (temp != NULL)
+	while (!has_nl(*lst))
 	{
-		//printf("[%d] - |%s|\n", i, temp->content);
-		i++;
-		temp = temp->next;
+		buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buffer)
+			return (clear_lst(lst));
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (!bytes_read)
+			return free(buffer);
+		buffer[bytes_read] = '\0';
+		if (!add_node(lst, buffer))
+			return (clear_lst(lst));
 	}
 }
-
-int	has_nl(t_list **lst)
+char	*save_line(t_list **lst)
 {
-	int		i;
-	t_list	*temp;
-
-	temp = *lst;
-	while (temp)
-	{
-		i = 0;
-		while (temp->content[i] != '\0')
-		{
-			if (temp->content[i] == '\n')
-				return (1);
-			i++;
-		}
-		temp = temp->next;
-	}
-	return (0);
-}
-int	lenght_nl(t_list **lst)
-{
-	int		i;
-	int len;
-	t_list	*temp;
-
-	temp = *lst;
-	len = 0;
-	while (temp)
-	{
-		i = 0;
-		while (temp->content[i] != '\0')
-		{
-			if (temp->content[i] == '\n')
-				break ;
-			i++;
-			len++;
-		}
-		temp = temp->next;
-	}
-	return(len);
-}
-char	*save_line(t_list **lst, int line_size)
-{
+	int		len;
+	t_list	*aux;
 	int		i;
 	int		j;
-	t_list	*temp;
 	char	*line;
 
-	temp = *lst;
+	i = 0;
 	j = 0;
-	//printf("Creating line size: %d\n",line_size);
-	line = malloc((line_size + 1) * sizeof(char*));
-	while (temp && j < line_size)
+	aux = *lst;
+	len = index_newline(*lst);
+	if (!len)
+		return (NULL);
+	line = malloc(sizeof(char) * (len + 1));
+	if (!line)
+		return (NULL);
+	while (aux)
 	{
-		i = 0;
-		while (temp->content[i] != '\0' && j <= line_size)
-		{
-			//printf("->%c\n", temp->content[i]);
-			line[j] = temp->content[i];
-			i++;
-			j++;
-		}
-		temp = temp->next;
+		while (aux->content[j] && i < len)
+			line[i++] = aux->content[j++];
+		j = 0;
+		aux = aux->next;
 	}
-	if(j==0)
-		return NULL;
-	line[j] = '\0';
-	//printf("Line created: |%s|\n", line);
-	//printf("j:%d\n", j);
+	line[i] = '\0';
 	return (line);
 }
-char	*next_line(int fd, t_list **lst)
-{
-	char	readed[BUFFER_SIZE + 1];
-	char	*line;
-	int		b_read;
 
-	readed[BUFFER_SIZE] = '\0';
-	b_read = 1;
-	line = "";
-	while (b_read > 0 && !has_nl(lst))
-	{
-		b_read = read(fd, readed, BUFFER_SIZE);
-		if (b_read < 0)
-		{
-			break ;
-		}
-		readed[b_read] = '\0';
-		//printf("Read: |%s| bytes: %d\n", readed, b_read);
-		lstadd_back(lst, lstnew(readed));
-		// printf("byts readed: %d\n", b_read);
-		lst_show(lst);
-	}
-	line = save_line(lst, lenght_nl(lst));
-	//lst = save_reminder(lst, has_nl(lst, 1))
-	return (line);
+void save_next(t_list **lst)
+{
+	t_list	*last;
+	char	*content;
+	int		start;
+	int		i;
+	int		len;
+
+	last = last_node(*lst);
+	len = 0;
+	start = index_newline(last);
+	while (last->content[start + len])
+		len++;
+	if (!len)
+		return (clear_lst(lst));
+	content = malloc(sizeof(char) * (len + 1));
+	if (!content)
+		return (clear_lst(lst));
+	i = 0;
+	while (last->content[start] && i < len)
+		content[i++] = last->content[start++];
+	content[i] = '\0';
+	clear_lst(lst);
+	if (!add_node(lst, content))
+		return;
 }
 
 char	*get_next_line(int fd)
 {
+	static t_list	*lst = NULL;
 	char			*line;
-	static t_list	*lst;
 
-	if (fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = next_line(fd, &lst);
+	line = NULL;
+	if (read(fd, line, 0) < 0)
+	{
+		clear_lst(&lst);
+		return (NULL);
+	}
+	read_fd(fd, &lst);
+	if (!lst)
+		return (NULL);
+	line = save_line(&lst);
 	if(!line)
-		free(line);
-	clean_lst(&lst);
+		clear_lst(&lst);
+	if (!lst)
+		return (NULL);
+	save_next(&lst);
 	return (line);
 }
+/*int main(void)
+{
+	int    fd;
+	char  *next_line = " ";
+	int  count;
+
+	count = 0;
+	printf("Init program\n");
+	fd = open("file.txt", O_RDONLY);
+	printf("File open succes!\n");
+	while (next_line){
+		next_line = get_next_line(fd);
+		printf("	Returned in [%d]: |%s|\n", count, next_line);
+		// free(next_line);
+		count++;
+	}
+	close(fd);
+	return (0);
+}*/
