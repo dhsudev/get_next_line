e1cebd0667193b574a7b65e4b1f01019fb4a906f
lua <ltrevin-@student.42barcelona.com>
2024-05-06 17:26:17 +0200

    all working 🎷🦦

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ltrevin- <ltrevin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:36:37 by ltrevin-          #+#    #+#             */
-/*   Updated: 2024/04/29 14:42:51 by ltrevin-         ###   ########.fr       */
+/*   Updated: 2024/05/06 17:25:02 by ltrevin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <stddef.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 2
+#  define BUFFER_SIZE 5
 # endif
 
 typedef struct s_list
@@ -28,7 +28,12 @@ typedef struct s_list
 }	t_list;
 
 char	*get_next_line(int fd);
-t_list	*lstnew(void *content);
-void	lstadd_back(t_list **lst, t_list *new);
-void	clean_lst(t_list **lst);
+void	save_next(t_list **lst);
+char	*save_line(t_list **lst);
+void	read_fd(int fd, t_list **lst);
+int		has_nl(t_list *lst);
+t_list	*last_node(t_list *lst);
+t_list	*add_node(t_list **lst, char *content);
+int		index_newline(t_list *lst);
+void	clear_lst(t_list **lst);
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,66 +6,97 @@
 /*   By: ltrevin- <ltrevin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:36:37 by ltrevin-          #+#    #+#             */
-/*   Updated: 2024/04/29 14:54:22 by ltrevin-         ###   ########.fr       */
+/*   Updated: 2024/05/06 17:25:21 by ltrevin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <string.h>
 
-void	lstadd_back(t_list **lst, t_list *new)
+int	has_nl(t_list *lst)
 {
-	t_list	*temp;
+	t_list	*last;
+	int		i;
 
-	if (*lst == NULL)
+	if (!lst)
+		return (0);
+	last = last_node(lst);
+	i = 0;
+	while (last->content[i] != '\0')
 	{
-		*lst = new;
-		return ;
+		if (last->content[i] == '\n')
+			return (1);
+		i++;
 	}
-	temp = *lst;
-	while (temp && temp->next)
-		temp = temp->next;
-	temp->next = new;
-	//printf("|%p|%p|\n", (*lst)->content, (*lst)->next->content);
+	return (0);
 }
 
-t_list	*lstnew(void *content)
+t_list	*last_node(t_list *lst)
 {
-	t_list	*node;
+	t_list	*tmp;
 
-	node = malloc(sizeof(t_list));
-	if (!node)
+	if (!lst)
 		return (NULL);
-	node->content = strdup(content);
-	node->next = NULL;
-	return (node);
+	tmp = lst;
+	while (tmp->next)
+		tmp = tmp->next;
+	return (tmp);
 }
-// char	*ft_strdup(const char *s1)
-// {
-// 	char	*s2;
-// 	size_t	size;
 
-// 	size = ft_strlen(s1);
-// 	s2 = malloc(size * sizeof(char) + 1);
-// 	if (!s2)
-// 		return (NULL);
-// 	ft_strlcpy(s2, s1, size + 1);
-// 	return (s2);
-// }
-void	clean_lst(t_list **lst)
+t_list	*add_node(t_list **lst, char *content)
+{
+	t_list	*new_node;
+	t_list	*last_node;
+
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
+		return (NULL);
+	new_node->content = content;
+	new_node->next = NULL;
+	if (*lst == NULL)
+		*lst = new_node;
+	else
+	{
+		last_node = *lst;
+		while (last_node->next != NULL)
+			last_node = last_node->next;
+		last_node->next = new_node;
+	}
+	return (new_node);
+}
+
+int	index_newline(t_list *lst)
+{
+	t_list	*tmp;
+	int		len;
+	int		i;
+
+	tmp = lst;
+	len = 0;
+	while (tmp)
+	{
+		i = 0;
+		while (tmp->content[i])
+		{
+			len++;
+			if (tmp->content[i] == '\n')
+				break ;
+			i++;
+		}
+		tmp = tmp->next;
+	}
+	return (len);
+}
+
+void	clear_lst(t_list **lst)
 {
 	t_list	*tmp;
 
 	while (*lst)
 	{
-		tmp = (**lst).next;
-		free((**lst).content);
+		tmp = (*lst)->next;
+		free((*lst)->content);
 		free(*lst);
 		*lst = tmp;
 	}
-	free(*lst);
 	*lst = NULL;
 }
-
-
